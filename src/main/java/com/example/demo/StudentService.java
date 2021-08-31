d1482815468b970f8385ba5a99020851cf59826f
CookJustin <cookjpaypal@gmail.com>
2021-08-31 16:30:24 -0400

    Created Postmapping for a Student to be added to our database.

--- src/main/java/com/example/demo/StudentService.java
+++ src/main/java/com/example/demo/StudentService.java
@@ -6,12 +6,16 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @AllArgsConstructor
-@Service //spring initailizes this class as a bean
+@Service //spring initializes this class as a bean
 public class StudentService {
 
     private final StudentRepository studentRepository;
 
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student){
+        studentRepository.save(student);
     }
 }
