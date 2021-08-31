427535b103c2519db2b09ebf0567fcf2b4239bb9
CookJustin <cookjpaypal@gmail.com>
2021-08-30 23:06:53 -0400

    Altered application.properties to require unique emails. Added @Indexed annotation to Student.java to require unique emails.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -18,17 +21,18 @@ public class DemoApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository){
+	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate){
 		return args -> {
 			Address address = new Address(
 					"England",
 					"London",
 					"NE9"
 			);
+			String email = "Jahmed@gmail.com";
 			Student student = new Student(
 					"Jamila",
 					"Ahmed",
-					"Jahmed@gmail.com",
+					email,
 					Gender.FEMALE,
 					address,
 					List.of("Computer Science","Maths"),
@@ -36,8 +40,22 @@ public class DemoApplication {
 					LocalDateTime.now()
 			);
 
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(email));
 
-			repository.insert(student);
+			List<Student> students = mongoTemplate.find(query, Student.class);
+
+			if(students.size() > 1){
+				throw new IllegalStateException("Duplicate Student Email " + email);
+			}
+			if(students.isEmpty()){
+				System.out.println("Inserting student " + student);
+				repository.insert(student);
+			}else{
+				System.out.println(student + " already exists");
+			}
+
+
 		};
 	}
 }
